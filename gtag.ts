fc5829db3f1299931ce88606a230e352806eb9a0
Konrad a <kowalik0konrad@gmail.com>
2023-06-22 10:38:10 +0200

    Gtag fix

---
+++ gtag.ts
@@ -0,0 +1,24 @@
+export const GA_TRACKING_ID = "G-9YWKG3KQE9" //replace it with your measurement id
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+    // @ts-ignore
+    window.gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+    })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: {
+    action: string,
+    category: string,
+    label: string,
+    value: number
+}) => {
+    // @ts-ignore
+    window.gtag("event", action, {
+        event_category: category,
+        event_label: label,
+        value: value,
+    })
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,55 @@ import {QueryClient} from "@tanstack/query-core";
 import {QueryClientProvider} from "@tanstack/react-query";
 import {useRouter} from "next/router";
 import Layout from "@/components/Shared/Layout";
+import {useEffect} from "react";
+import Script from "next/script"
+import * as gtag from '@/gtag'
 
 const queryClient = new QueryClient()
 
 export default function App({Component, pageProps}: AppProps) {
-   const router = useRouter();
-   const layoutPaths = ["/", "/storyboardy", "/policies"];
-   const isLayout = layoutPaths.includes(router.pathname);
-   return (
-      <QueryClientProvider client={queryClient}>
-         {isLayout ? (
-            <Layout>
-               <Component {...pageProps} />
-            </Layout>
-         ) : (
-            <Component {...pageProps} />
-         )}
-      </QueryClientProvider>
-   )
+    const router = useRouter();
+    const layoutPaths = ["/", "/storyboardy", "/policies"];
+    const isLayout = layoutPaths.includes(router.pathname);
+
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            gtag.pageview(url)
+        }
+        router.events.on("routeChangeComplete", handleRouteChange)
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange)
+        }
+    }, [router.events])
+    return (
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+            />
+            <QueryClientProvider client={queryClient}>
+                {isLayout ? (
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                ) : (
+                    <Component {...pageProps} />
+                )}
+            </QueryClientProvider>
+        </>
+    )
 }
