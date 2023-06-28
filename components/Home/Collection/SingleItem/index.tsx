8bc6445a994ac4bcb64925903a3baa456f4f3e8d
Konrad a <kowalik0konrad@gmail.com>
2023-06-28 09:58:59 +0200

    Favicons

--- components/Home/Collection/SingleItem/index.tsx
+++ components/Home/Collection/SingleItem/index.tsx
@@ -5,38 +5,46 @@ import {getSimpleImageUri} from "@/utils/getSimpleImageUri";
 import Link from "next/link";
 
 export type SingleItemT = {
-   title: string;
-   shopLink: string;
-   price: string;
-   cover: StrapiFile;
+    title: string;
+    shopLink: string;
+    price: string;
+    cover: StrapiFile;
+    soldOutBadge?: boolean;
+    comingSoonBadge?: boolean;
+    newBadge?: boolean;
 }
 
 export default function SingleItem({content}: { content: SingleItemT }) {
-   const {title, shopLink, price, cover} = content;
-   return (
-      <article className={s.wrapper}>
-         <Link
-            href={shopLink}
-            target={"_blank"}
-         >
-            <div className={s.imageWrapper}>
-               <Image
-                  src={getSimpleImageUri(cover)}
-                  alt={title}
-                  width={375}
-                  height={375}
-                  priority
-               />
-            </div>
-            <div className={s.bottomWrapper}>
-               <div>
-                  <p className={s.title}>{title}</p>
-                  <p className={s.price}>{price}</p>
-               </div>
+    const {title, shopLink, price, cover, soldOutBadge, comingSoonBadge, newBadge} = content;
+    return (
+        <article className={s.wrapper}>
+            <Link
+                href={shopLink}
+                target={"_blank"}
+            >
+                <div className={s.badgeWrapper}>
+                    {soldOutBadge && <p className={s.soldOutBadge}>SOLD OUT</p>}
+                    {comingSoonBadge && <p className={s.comingSoonBadge}>COMING SOON</p>}
+                    {newBadge && <p className={s.newBadge}>NEW</p>}
+                </div>
+                <div className={s.imageWrapper}>
+                    <Image
+                        src={getSimpleImageUri(cover)}
+                        alt={title}
+                        width={375}
+                        height={375}
+                        priority
+                    />
+                </div>
+                <div className={s.bottomWrapper}>
+                    <div>
+                        <p className={s.title}>{title}</p>
+                        <p className={s.price}>{price}</p>
+                    </div>
 
-               <span>Check out</span>
-            </div>
-         </Link>
-      </article>
-   )
+                    <span>Check out</span>
+                </div>
+            </Link>
+        </article>
+    )
 }
--- components/Shared/Layout/index.tsx
+++ components/Shared/Layout/index.tsx
@@ -6,52 +6,62 @@ import {useLoading} from "@/hooks/useLoading";
 import Navbar from "@/components/Shared/Navbar";
 import {createContext, ReactNode} from "react";
 import Footer from "@/components/Shared/Footer";
+import Head from "next/head";
 
-export const DataContext = createContext<{data: any}>({data: {}});
+export const DataContext = createContext<{ data: any }>({data: {}});
 
 export default function Layout({children}: { children: ReactNode }) {
-   const {data, isLoading, isError} = useQuery({queryKey: ['shared'], queryFn: () => getData("/shared")})
+    const {data, isLoading, isError} = useQuery({queryKey: ['shared'], queryFn: () => getData("/shared")})
 
-   const loading = useLoading(isLoading);
+    const loading = useLoading(isLoading);
 
-   if (loading) {
-      return <Loading/>
-   }
+    if (loading) {
+        return <Loading/>
+    }
 
-   if (isError) {
-      return <ErrorComponent/>
-   }
+    if (isError) {
+        return <ErrorComponent/>
+    }
 
-   const {
-      nav,
-      facebookLink,
-      instagramLink,
-      youtubeLink,
-      tiktokLink,
-      phoneNumber,
-      nipNumber,
-      address,
-      logoFooter,
-   } = data.data.attributes;
+    const {
+        nav,
+        facebookLink,
+        instagramLink,
+        youtubeLink,
+        tiktokLink,
+        phoneNumber,
+        nipNumber,
+        address,
+        logoFooter,
+    } = data.data.attributes;
 
-   const footerContent = {
-      facebookLink,
-      instagramLink,
-      address,
-      phoneNumber,
-      nipNumber,
-      logoFooter,
-      youtubeLink
-   }
-   return (
-      <>
-         <Navbar content={{nav, facebookLink, youtubeLink, instagramLink, tiktokLink}}/>
+    const footerContent = {
+        facebookLink,
+        instagramLink,
+        address,
+        phoneNumber,
+        nipNumber,
+        logoFooter,
+        youtubeLink
+    }
+    return (
+        <>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="msapplication-TileColor" content="#da532c"/>
+                <meta name="theme-color" content="#ffffff"/>
+            </Head>
+            <Navbar content={{nav, facebookLink, youtubeLink, instagramLink, tiktokLink}}/>
 
-         <DataContext.Provider value={{data: data.data.attributes}}>
-            {children}
-         </DataContext.Provider>
+            <DataContext.Provider value={{data: data.data.attributes}}>
+                {children}
+            </DataContext.Provider>
 
-         <Footer content={footerContent}/>
-      </>
-   )
+            <Footer content={footerContent}/>
+        </>
+    )
 }
