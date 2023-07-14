3f54b14736c0a447e1e23050e81264df0a2f7fad
c0ndi <kowalik0konrad@gmail.com>
2023-07-14 14:24:54 +0200

    fix

--- components/Shared/Seo/index.tsx
+++ components/Shared/Seo/index.tsx
@@ -5,164 +5,164 @@ import {StrapiFile} from "@/types/types";
 import Head from "next/head";
 
 type Seo = {
-   metaTitle: string;
-   metaDescription: string;
-   metaImage: string;
-   metaSocialFacebook: {
-      title: string;
-      description: string;
-      image: StrapiFile;
-      url: string;
-   };
-   metaSocialTwitter: {
-      title: string;
-      description: string;
-      image: StrapiFile;
-      url: string;
-   };
-   keywords?: string;
-   metaRobots?: string;
-   structuredData?: string;
-   metaViewport?: string;
-   canonicalURL?: string;
+  metaTitle: string;
+  metaDescription: string;
+  metaImage: string;
+  metaSocialFacebook: {
+    title: string;
+    description: string;
+    image: StrapiFile;
+    url: string;
+  };
+  metaSocialTwitter: {
+    title: string;
+    description: string;
+    image: StrapiFile;
+    url: string;
+  };
+  keywords?: string;
+  metaRobots?: string;
+  structuredData?: string;
+  metaViewport?: string;
+  canonicalURL?: string;
 }
 
 export default function Seo({seo}: PropsWithChildren<{ seo: Seo }>) {
-   const seoData = seo;
-   const seoStructuredData = seo?.structuredData;
-   return (
-      <Head>
-          <meta name="robots" content="all" />
-         {seoData &&
-            <>
-               <title>{seoData.metaTitle}</title>
-               <meta
+  const seoData = seo;
+  const seoStructuredData = seo?.structuredData;
+  return (
+    <Head>
+      <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"/>
+      {seoData &&
+          <>
+              <title>{seoData.metaTitle}</title>
+              <meta
                   name="description"
                   content={seoData.metaDescription}
                   key="description"
-               />
-               <meta
+              />
+              <meta
                   name="keywords"
                   content={seoData.keywords}
-               />
-               <meta
+              />
+              <meta
                   name="twitter:card"
                   content="summary_large_image"
                   key="twitter:card"
-               />
-               <meta
+              />
+              <meta
                   property="og:url"
                   content={seoData.canonicalURL}
                   key="og:url"
-               />
-               <meta
+              />
+              <meta
                   property="og:title"
                   content={seoData.metaTitle}
                   key="og:title"
-               />
-               <meta
+              />
+              <meta
                   property="og:description"
                   content={seoData.metaDescription}
                   key="og:description"
-               />
-               <meta
+              />
+              <meta
                   property="og:image"
                   content={strapiURL + seoData?.metaImage}
                   key="og:image"
-               />
-               <link
+              />
+              <link
                   rel="canonical"
                   href={seoData.canonicalURL}
-               />
-            </>
-         }
+              />
+          </>
+      }
 
-         {seoData?.metaSocialFacebook && (
-            <>
-               <meta
-                  property="og:title"
-                  content={seoData.metaSocialFacebook.title}
-               />
-               <meta
-                  property="og:url"
-                  content={seoData.metaSocialFacebook.url}
-               />
-               <meta
-                  property="og:description"
-                  content={seoData.metaSocialFacebook.description}
-               />
-               <meta
-                  property="og:image"
-                  content={strapiURL + seoData.metaSocialFacebook.image?.data.attributes.url}
-               />
-               <meta
-                  property="og:type"
-                  content="article"
-               />
-            </>
-         )}
+      {seoData?.metaSocialFacebook && (
+        <>
+          <meta
+            property="og:title"
+            content={seoData.metaSocialFacebook.title}
+          />
+          <meta
+            property="og:url"
+            content={seoData.metaSocialFacebook.url}
+          />
+          <meta
+            property="og:description"
+            content={seoData.metaSocialFacebook.description}
+          />
+          <meta
+            property="og:image"
+            content={strapiURL + seoData.metaSocialFacebook.image?.data.attributes.url}
+          />
+          <meta
+            property="og:type"
+            content="article"
+          />
+        </>
+      )}
 
-         {seoData?.metaSocialTwitter && (
-            <>
-               <meta
-                  property="og:title"
-                  content={seoData.metaSocialTwitter.title}
-               />
-               <meta
-                  property="og:url"
-                  content={seoData.metaSocialTwitter.url}
-               />
-               <meta
-                  property="og:description"
-                  content={seoData.metaSocialTwitter.description}
-               />
-               <meta
-                  property="og:image"
-                  content={strapiURL + seoData.metaSocialTwitter.image?.data.attributes.url}
-               />
-               <meta
-                  property="og:type"
-                  content="article"
-               />
-            </>
-         )}
+      {seoData?.metaSocialTwitter && (
+        <>
+          <meta
+            property="og:title"
+            content={seoData.metaSocialTwitter.title}
+          />
+          <meta
+            property="og:url"
+            content={seoData.metaSocialTwitter.url}
+          />
+          <meta
+            property="og:description"
+            content={seoData.metaSocialTwitter.description}
+          />
+          <meta
+            property="og:image"
+            content={strapiURL + seoData.metaSocialTwitter.image?.data.attributes.url}
+          />
+          <meta
+            property="og:type"
+            content="article"
+          />
+        </>
+      )}
 
-         <script type="application/ld+json">{seoStructuredData}</script>
+      <script type="application/ld+json">{seoStructuredData}</script>
 
-         <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-         />
-         <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-         />
-         <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-         />
-         <link
-            rel="manifest"
-            href="/site.webmanifest"
-         />
-         <link
-            rel="mask-icon"
-            href="/safari-pinned-tab.svg"
-            color="#5bbad5"
-         />
-         <meta
-            name="msapplication-TileColor"
-            content="#da532c"
-         />
-         <meta
-            name="theme-color"
-            content="#ffffff"
-         />
-      </Head>
-   )
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="manifest"
+        href="/site.webmanifest"
+      />
+      <link
+        rel="mask-icon"
+        href="/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta
+        name="msapplication-TileColor"
+        content="#da532c"
+      />
+      <meta
+        name="theme-color"
+        content="#ffffff"
+      />
+    </Head>
+  )
 }
