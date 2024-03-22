e596ba652585a51711a305bb0601dd3cfb8b6714
c0ndi <kowalik0konrad@gmail.com>
2024-03-22 18:39:06 +0100

    fix

--- components/Storyboard/Storyboards/index.tsx
+++ components/Storyboard/Storyboards/index.tsx
@@ -1,23 +1,22 @@
-import s from './index.module.scss'
-import {Storyboard} from "@/pages/storyboardy";
-import CoversSlider from "@/components/Storyboard/StoryboardsCover";
-import StoryboardCover from "@/components/Storyboard/StoryboardsCover";
+// import s from './index.module.scss'
+// import CoversSlider from "@/components/Storyboard/StoryboardsCover";
+// import StoryboardCover from "@/components/Storyboard/StoryboardsCover";
 
-type Storyboards = {
-   storyboards: { data: Storyboard [] };
-}
+// type Storyboards = {
+//    storyboards: { data: Storyboard[] };
+// }
 
-export default function Storyboards({storyboards}: Storyboards) {
-   return (
-      <section className={s.wrapper}>
-         {storyboards.data.map((storyboard, index) => (
-            <StoryboardCover
-               singleCover={storyboard.attributes.singleCover}
-               title={storyboard.attributes.title}
-               slug={storyboard.attributes.slug}
-               key={index}
-            />
-         ))}
-      </section>
-   )
-}
+// export default function Storyboards({ storyboards }: Storyboards) {
+//    return (
+//       <section className={s.wrapper}>
+//          {storyboards.data.map((storyboard, index) => (
+//             <StoryboardCover
+//                singleCover={storyboard.attributes.singleCover}
+//                title={storyboard.attributes.title}
+//                slug={storyboard.attributes.slug}
+//                key={index}
+//             />
+//          ))}
+//       </section>
+//    )
+// }
--- components/Storyboard/TableOfContents/index.tsx
+++ components/Storyboard/TableOfContents/index.tsx
@@ -1,23 +1,22 @@
-import s from './index.module.scss'
-import Link from "next/link";
-import {Storyboard} from "@/pages/storyboardy";
+// import s from './index.module.scss'
+// import Link from "next/link";
 
 
-type TableOfContents = {
-   content: {data: Storyboard []};
-}
+// type TableOfContents = {
+//    content: { data: Storyboard[] };
+// }
 
-export default function TableOfContents({content}: TableOfContents) {
-   return (
-      <section className={s.wrapper}>
-         {content.data.map((storyboard, index) => (
-            <Link
-               key={index}
-               href={`/storyboardy#${storyboard.attributes.slug}`}
-            >
-               <p>{storyboard.attributes.title.toUpperCase()}</p>
-            </Link>
-         ))}
-      </section>
-   )
-}
+// export default function TableOfContents({ content }: TableOfContents) {
+//    return (
+//       <section className={s.wrapper}>
+//          {content.data.map((storyboard, index) => (
+//             <Link
+//                key={index}
+//                href={`/storyboardy#${storyboard.attributes.slug}`}
+//             >
+//                <p>{storyboard.attributes.title.toUpperCase()}</p>
+//             </Link>
+//          ))}
+//       </section>
+//    )
+// }
