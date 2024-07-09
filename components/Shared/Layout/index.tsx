4f25e37d1c0e6b4fbfadf9d4e1b974de5db83a6d
c0ndi <kowalik0konrad@gmail.com>
2024-07-09 21:45:46 +0200

    fix

--- components/Shared/Layout/index.tsx
+++ components/Shared/Layout/index.tsx
@@ -1,13 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
-import { getData } from "@/utils/getData";
-import Loading from "@/components/Shared/Loading";
 import ErrorComponent from "@/components/Shared/ErrorComponent";
-import { useLoading } from "@/hooks/useLoading";
-import Navbar from "@/components/Shared/Navbar";
-import { createContext, ReactNode } from "react";
 import Footer from "@/components/Shared/Footer";
-import Head from "next/head";
+import Loading from "@/components/Shared/Loading";
+import Navbar from "@/components/Shared/Navbar";
+import { useLoading } from "@/hooks/useLoading";
+import { getData } from "@/utils/getData";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { createContext, ReactNode } from "react";
 
 export const DataContext = createContext<{ data: any }>({ data: {} });
 
@@ -48,15 +47,6 @@ export default function Layout({ children }: { children: ReactNode }) {
     }
     return (
         <>
-            <Head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-                <meta name="msapplication-TileColor" content="#da532c" />
-                <meta name="theme-color" content="#ffffff" />
-            </Head>
             <Navbar content={{ nav, facebookLink, youtubeLink, instagramLink, tiktokLink }} />
 
             <DataContext.Provider value={{ data: data.data.attributes }}>
--- components/Shared/Seo/index.tsx
+++ components/Shared/Seo/index.tsx
@@ -46,6 +46,34 @@ export default function Seo({ seo }: PropsWithChildren<{ seo: Seo }>) {
           alt: 'Home - Falko Project',
         }]
       }}
+      additionalLinkTags={[
+        {
+          rel: "icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+        {
+          rel: "mask-icon",
+          href: "/safari-pinned-tab.svg",
+          color: "#5bbad5",
+        },
+      ]}
     />
   )
   // }
