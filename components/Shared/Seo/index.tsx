b7d71db1a6cf538ce26d099ba87f765b59f22856
c0ndi <kowalik0konrad@gmail.com>
2024-07-09 21:27:00 +0200

    fux

--- components/Shared/Seo/index.tsx
+++ components/Shared/Seo/index.tsx
@@ -1,5 +1,5 @@
 import { StrapiFile } from "@/types/types";
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 import { PropsWithChildren } from "react";
 
 type Seo = {
@@ -28,133 +28,32 @@ type Seo = {
 export default function Seo({ seo }: PropsWithChildren<{ seo: Seo }>) {
   const seoData = seo;
   const seoStructuredData = seo?.structuredData;
+
+  if (!seoData) {
+    return (
+      <NextSeo
+        title="Home - Falko Project"
+        description='let me be your guide through the story of redefined fashion and crumbled world'
+        canonical="https://web.falkoproject.com/en"
+        openGraph={{
+          url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+          title: 'Home - Falko Project',
+          description: 'let me be your guide through the story of redefined fashion and crumbled world',
+        }}
+      />
+    )
+  }
+
   return (
-    <Head>
-      <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
-      <meta
-        property="og:image"
-        content={"https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg"}
-        key="og:image"
-      />
-      {seoData &&
-        <>
-          <title>{seoData.metaTitle}</title>
-          <meta
-            name="description"
-            content={seoData.metaDescription}
-            key="description"
-          />
-          <meta
-            name="keywords"
-            content={seoData.keywords}
-          />
-          <meta
-            name="twitter:card"
-            content="summary_large_image"
-            key="twitter:card"
-          />
-          <meta
-            property="og:url"
-            content={seoData.canonicalURL}
-            key="og:url"
-          />
-          <meta
-            property="og:title"
-            content={seoData.metaTitle}
-            key="og:title"
-          />
-          <meta
-            property="og:description"
-            content={seoData.metaDescription}
-            key="og:description"
-          />
-          <link
-            rel="canonical"
-            href={seoData.canonicalURL}
-          />
-        </>
-      }
-
-
-      {seoData?.metaSocialFacebook && (
-        <>
-          <meta
-            property="og:title"
-            content={seoData.metaSocialFacebook.title}
-          />
-          <meta
-            property="og:url"
-            content={seoData.metaSocialFacebook.url}
-          />
-          <meta
-            property="og:description"
-            content={seoData.metaSocialFacebook.description}
-          />
-          <meta
-            property="og:type"
-            content="article"
-          />
-        </>
-      )}
-
-      {seoData?.metaSocialTwitter && (
-        <>
-          <meta
-            property="og:title"
-            content={seoData.metaSocialTwitter.title}
-          />
-          <meta
-            property="og:url"
-            content={seoData.metaSocialTwitter.url}
-          />
-          <meta
-            property="og:description"
-            content={seoData.metaSocialTwitter.description}
-          />
-          <meta
-            property="og:type"
-            content="article"
-          />
-        </>
-      )}
-
-
-      <script type="application/ld+json">{seoStructuredData}</script>
-
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link
-        rel="manifest"
-        href="/site.webmanifest"
-      />
-      <link
-        rel="mask-icon"
-        href="/safari-pinned-tab.svg"
-        color="#5bbad5"
-      />
-      <meta
-        name="msapplication-TileColor"
-        content="#da532c"
-      />
-      <meta
-        name="theme-color"
-        content="#ffffff"
-      />
-    </Head>
+    <NextSeo
+      title={seo.metaTitle ?? 'Home - Falko Project'}
+      description={seo.metaDescription ?? 'let me be your guide through the story of redefined fashion and crumbled world'}
+      canonical={seo.canonicalURL ?? 'https://web.falkoproject.com/en'}
+      openGraph={{
+        url: seo.metaImage ?? 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+        title: seo.metaTitle,
+        description: seo.metaDescription,
+      }}
+    />
   )
 }
