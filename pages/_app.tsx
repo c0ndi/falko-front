6b32d5be0f912a517d664e04b05102be28725950
c0ndi <kowalik0konrad@gmail.com>
2024-07-09 22:01:01 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '@/styles/reset.scss';
 import '@/styles/shared.scss';
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { NextSeo } from "next-seo";
 import type { AppProps } from 'next/app';
 import { useRouter } from "next/router";
 import Script from "next/script";
@@ -28,6 +29,50 @@ export default function App({ Component, pageProps }: AppProps) {
     }, [router.events])
     return (
         <>
+            <NextSeo
+                title="Home - Falko Project"
+                description='let me be your guide through the story of redefined fashion and crumbled world'
+                canonical="https://web.falkoproject.com/en"
+                openGraph={{
+                    url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+                    title: 'Home - Falko Project',
+                    description: 'let me be your guide through the story of redefined fashion and crumbled world',
+                    images: [{
+                        url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+                        width: 850,
+                        height: 650,
+                        alt: 'Home - Falko Project',
+                    }]
+                }}
+                additionalLinkTags={[
+                    {
+                        rel: "icon",
+                        sizes: "180x180",
+                        href: "/apple-touch-icon.png",
+                    },
+                    {
+                        rel: "icon",
+                        type: "image/png",
+                        sizes: "32x32",
+                        href: "/favicon-32x32.png",
+                    },
+                    {
+                        rel: "icon",
+                        type: "image/png",
+                        sizes: "16x16",
+                        href: "/favicon-16x16.png",
+                    },
+                    {
+                        rel: "manifest",
+                        href: "/site.webmanifest",
+                    },
+                    {
+                        rel: "mask-icon",
+                        href: "/safari-pinned-tab.svg",
+                        color: "#5bbad5",
+                    },
+                ]}
+            />
             <Script
                 strategy="afterInteractive"
                 src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
