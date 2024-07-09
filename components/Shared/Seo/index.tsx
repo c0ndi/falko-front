26e595eb77c6f84da9e71eae6c4d7abb1afe4383
c0ndi <kowalik0konrad@gmail.com>
2024-07-09 22:00:38 +0200

    fix

--- components/Shared/Seo/index.tsx
+++ components/Shared/Seo/index.tsx
@@ -1,6 +1,4 @@
 import { StrapiFile } from "@/types/types";
-import { getSimpleImageUri } from "@/utils/getSimpleImageUri";
-import { NextSeo } from "next-seo";
 import { PropsWithChildren } from "react";
 
 type Seo = {
@@ -29,100 +27,101 @@ type Seo = {
 export default function Seo({ seo }: PropsWithChildren<{ seo: Seo }>) {
   const seoData = seo;
   const seoStructuredData = seo?.structuredData;
+  return null;
 
-  if (!seoData) {
-    return (
-      <NextSeo
-        title="Home - Falko Project"
-        description='let me be your guide through the story of redefined fashion and crumbled world'
-        canonical="https://web.falkoproject.com/en"
-        openGraph={{
-          url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
-          title: 'Home - Falko Project',
-          description: 'let me be your guide through the story of redefined fashion and crumbled world',
-          images: [{
-            url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
-            width: 850,
-            height: 650,
-            alt: 'Home - Falko Project',
-          }]
-        }}
-        additionalLinkTags={[
-          {
-            rel: "icon",
-            sizes: "180x180",
-            href: "/apple-touch-icon.png",
-          },
-          {
-            rel: "icon",
-            type: "image/png",
-            sizes: "32x32",
-            href: "/favicon-32x32.png",
-          },
-          {
-            rel: "icon",
-            type: "image/png",
-            sizes: "16x16",
-            href: "/favicon-16x16.png",
-          },
-          {
-            rel: "manifest",
-            href: "/site.webmanifest",
-          },
-          {
-            rel: "mask-icon",
-            href: "/safari-pinned-tab.svg",
-            color: "#5bbad5",
-          },
-        ]}
-      />
-    )
-  }
+  // if (!seoData) {
+  //   return (
+  //     <NextSeo
+  //       title="Home - Falko Project"
+  //       description='let me be your guide through the story of redefined fashion and crumbled world'
+  //       canonical="https://web.falkoproject.com/en"
+  //       openGraph={{
+  //         url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+  //         title: 'Home - Falko Project',
+  //         description: 'let me be your guide through the story of redefined fashion and crumbled world',
+  //         images: [{
+  //           url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+  //           width: 850,
+  //           height: 650,
+  //           alt: 'Home - Falko Project',
+  //         }]
+  //       }}
+  //       additionalLinkTags={[
+  //         {
+  //           rel: "icon",
+  //           sizes: "180x180",
+  //           href: "/apple-touch-icon.png",
+  //         },
+  //         {
+  //           rel: "icon",
+  //           type: "image/png",
+  //           sizes: "32x32",
+  //           href: "/favicon-32x32.png",
+  //         },
+  //         {
+  //           rel: "icon",
+  //           type: "image/png",
+  //           sizes: "16x16",
+  //           href: "/favicon-16x16.png",
+  //         },
+  //         {
+  //           rel: "manifest",
+  //           href: "/site.webmanifest",
+  //         },
+  //         {
+  //           rel: "mask-icon",
+  //           href: "/safari-pinned-tab.svg",
+  //           color: "#5bbad5",
+  //         },
+  //       ]}
+  //     />
+  //   )
+  // }
 
-  return (
-    <NextSeo
-      title={seoData.metaTitle ?? 'Home - Falko Project'}
-      description={seoData.metaDescription ?? 'let me be your guide through the story of redefined fashion and crumbled world'}
-      canonical={seoData.canonicalURL ?? 'https://web.falkoproject.com/en'}
-      openGraph={{
-        url: getSimpleImageUri(seoData.metaImage) ?? 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
-        title: seoData.metaTitle,
-        description: seoData.metaDescription,
-        images: [{
-          url: getSimpleImageUri(seoData.metaImage) ?? 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
-          width: 850,
-          height: 650,
-          alt: 'Home - Falko Project',
-        }]
-      }}
-      additionalLinkTags={[
-        {
-          rel: "icon",
-          sizes: "180x180",
-          href: "/apple-touch-icon.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          href: "/favicon-32x32.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          href: "/favicon-16x16.png",
-        },
-        {
-          rel: "manifest",
-          href: "/site.webmanifest",
-        },
-        {
-          rel: "mask-icon",
-          href: "/safari-pinned-tab.svg",
-          color: "#5bbad5",
-        },
-      ]}
-    />
-  )
+  // return (
+  //   <NextSeo
+  //     title={seoData.metaTitle ?? 'Home - Falko Project'}
+  //     description={seoData.metaDescription ?? 'let me be your guide through the story of redefined fashion and crumbled world'}
+  //     canonical={seoData.canonicalURL ?? 'https://web.falkoproject.com/en'}
+  //     openGraph={{
+  //       url: getSimpleImageUri(seoData.metaImage) ?? 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+  //       title: seoData.metaTitle,
+  //       description: seoData.metaDescription,
+  //       images: [{
+  //         url: getSimpleImageUri(seoData.metaImage) ?? 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+  //         width: 850,
+  //         height: 650,
+  //         alt: 'Home - Falko Project',
+  //       }]
+  //     }}
+  //     additionalLinkTags={[
+  //       {
+  //         rel: "icon",
+  //         sizes: "180x180",
+  //         href: "/apple-touch-icon.png",
+  //       },
+  //       {
+  //         rel: "icon",
+  //         type: "image/png",
+  //         sizes: "32x32",
+  //         href: "/favicon-32x32.png",
+  //       },
+  //       {
+  //         rel: "icon",
+  //         type: "image/png",
+  //         sizes: "16x16",
+  //         href: "/favicon-16x16.png",
+  //       },
+  //       {
+  //         rel: "manifest",
+  //         href: "/site.webmanifest",
+  //       },
+  //       {
+  //         rel: "mask-icon",
+  //         href: "/safari-pinned-tab.svg",
+  //         color: "#5bbad5",
+  //       },
+  //     ]}
+  //   />
+  // )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import Collection from "@/components/Home/Collection";
 import Hero from "@/components/Home/Hero";
 import OurManga from "@/components/Home/OurManga";
 import OurStoryboards from "@/components/Home/OurStoryboards";
-import Seo from "@/components/Shared/Seo";
 import { getData } from "@/utils/getData";
+import { NextSeo } from "next-seo";
 
 export default function Home({ data }: any) {
    const {
@@ -18,7 +18,50 @@ export default function Home({ data }: any) {
 
    return (
       <>
-         <Seo seo={seo} />
+         <NextSeo
+            title="Home - Falko Project"
+            description='let me be your guide through the story of redefined fashion and crumbled world'
+            canonical="https://web.falkoproject.com/en"
+            openGraph={{
+               url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+               title: 'Home - Falko Project',
+               description: 'let me be your guide through the story of redefined fashion and crumbled world',
+               images: [{
+                  url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+                  width: 850,
+                  height: 650,
+                  alt: 'Home - Falko Project',
+               }]
+            }}
+            additionalLinkTags={[
+               {
+                  rel: "icon",
+                  sizes: "180x180",
+                  href: "/apple-touch-icon.png",
+               },
+               {
+                  rel: "icon",
+                  type: "image/png",
+                  sizes: "32x32",
+                  href: "/favicon-32x32.png",
+               },
+               {
+                  rel: "icon",
+                  type: "image/png",
+                  sizes: "16x16",
+                  href: "/favicon-16x16.png",
+               },
+               {
+                  rel: "manifest",
+                  href: "/site.webmanifest",
+               },
+               {
+                  rel: "mask-icon",
+                  href: "/safari-pinned-tab.svg",
+                  color: "#5bbad5",
+               },
+            ]}
+         />
          <Hero content={hero} />
          <Collection content={collection} />
          <OurManga
