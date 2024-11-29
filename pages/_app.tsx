4828da3bfcdc0fd27a501c19f6470b97be5201d8
c0ndi <kowalik0konrad@gmail.com>
2024-11-30 00:45:56 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,11 +34,10 @@ export default function App({ Component, pageProps }: AppProps) {
                 description='let me be your guide through the story of redefined fashion and crumbled world'
                 canonical="https://falkoproject.com/en"
                 openGraph={{
-                    url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
                     title: 'Home - Falko Project',
                     description: 'let me be your guide through the story of redefined fashion and crumbled world',
                     images: [{
-                        url: 'https://strapi.falkoproject.com/uploads/431057761_1130037631464498_2642802240976394413_n_d5792f8824.jpg',
+                        url: '/images/og.jpg',
                         width: 850,
                         height: 650,
                         alt: 'Home - Falko Project',
